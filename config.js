4a34a7d3cd8f4c29436e919dd73a557528cd625b
Daniel Stolpe <daniel.stolpe@gmx.de>
2018-10-19 20:09:03 +0200

    initial

---
+++ config.js
@@ -0,0 +1,12 @@
+// Set your start and end dates.
+firstDate = new Date(2018, 10 - 1,  11),
+expectedDate = new Date(2018, 12 -1, 8, 23, 59, 59),
+// For expectedWords, expectedWordsPhd, expectedWordsMaster, and
+// expectedWordsPhd are defined.
+// For expectedPages, expectedPagesPhd, expectedPagesMaster, and
+// expectedPagesPhd are defined.
+// ... but you can also set your own
+expectedWords = expectedWordsMaster,
+expectedPages = expectedPagesMaster;
+// Set your name
+authorName = "Daniel"
